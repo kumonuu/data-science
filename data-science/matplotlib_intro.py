ee551f6f13c042bef821ba8224b79a4eee34989c
kumonuu <kumonuu@proton.me>
2026-06-27 13:01:25 +0100

    Add files via upload

--- data-science/matplotlib_intro.py
+++ data-science/matplotlib_intro.py
@@ -5,10 +5,17 @@ y = [1,4,9,16,25]
 
 plt.figure(figsize=(5,5))
 plt.title("graph")
-plt.scatter(x,y,label="quadratic")
-plt.scatter([1,2,3,4,5],[1,8,27,64,125],label="cubic")
+plt.plot(x,y,'b--',label="quadratic")
+plt.plot([1,2,3,4,5],[1,8,27,64,125],'r-.',label="cubic")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.legend()
 
-plt.savefig("figure.png")
+
+#plt.savefig("figure.png")
+
+plt.figure(figsize=(5,5))
+plt.title("bar graph")
+#plt.bar([1,3,5,7,9],[2,4,6,8,10])
+plt.bar(["Male","Female"],[23,41])
+plt.show()
