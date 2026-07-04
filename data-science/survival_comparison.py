c8bb7aeaddeb7f5a26621115aee94b0e11b10ceb
kumonuu <kumonuu@proton.me>
2026-07-04 12:58:41 +0100

    Add files via upload

---
+++ data-science/survival_comparison.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data-science/titanic.csv")
+group_by_sex = df.groupby(["Sex","Survived"]).size().unstack()
+print(group_by_sex[0])
+print(group_by_sex[1])
+
+# plt.figure(figsize=(5,5))
+# plt.title("Survival Comparison")
+# plt.bar([0,1],group_by_sex[0],width=0.5,label="Did not survive")
+# plt.bar([2,3],group_by_sex[1],width=0.5,label="Survived")
+# plt.legend()
+
+# plt.show()
+
+values = df["Sex"].value_counts()
+print(values)
+
+plt.figure(figsize=(5,5))
+plt.title("Pie Chart")
+plt.pie(values,autopct='%.1f%%',labels=values.index)
+plt.show()
